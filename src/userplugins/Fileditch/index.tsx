d1487f3720596f9bb7cf6f9b3d276a6d03d65a10
rxb3n <rubenkonan@proton.me>
2026-07-10 14:01:11 +0200

    header

--- src/userplugins/Fileditch/index.tsx
+++ src/userplugins/Fileditch/index.tsx
@@ -1,5 +1,5 @@
 /*
- * Vencord, a modification for Discord's desktop app
+ * Vencord, a Discord client mod
  * Copyright (c) 2024 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
